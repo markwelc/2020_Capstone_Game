9bcefd9153bf16ef7b169d2503d06112fc760a66
markwelc <markwelc@oregonstate.edu>
2020-08-31 16:46:27 -0700

    added in jumping (wanted more complex, didn't it working)

--- Dance_of_Warriors/Assets/Characters/Character.cs
+++ Dance_of_Warriors/Assets/Characters/Character.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    //only use this class for things that are universal for all characters (the player, bosses, npcs, rats)
+    //anything that's related to decision making should be in subclasses
+    //for instance, bosses decide when/where to move very differently from the player, and so deciding when/where to move is in a subclass
 
     public float maxHealth; //the amount of health that the character can have
     public float health; //the amount of health that the character has
 
     public float speed;//the default speed of the character
 
-    protected Rigidbody rb; //the character's rigidbody
+    protected Rigidbody characterRigidbody; //the character's rigidbody
     protected Transform characterTransform; //the character's transform
+    protected Collider characterCollider; //the character's collider
 
     protected Vector3 movement;//used to hold the direction that the character should move in
     protected bool diagonal;//whether the player is moving diagonally
+    [SerializeField] protected float jumpForce; //this is how strongly the character can jump
+    protected bool canJump; //determines if the character can currently jump
+    //[SerializeField] protected int jumpHeight; //used to store the max height of the jump (uses the jumpLeft variable)
+    //[SerializeField] protected int jumpLeft; //how much jumping force the character has in them
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = this.GetComponent<Rigidbody>(); //get rigidbody
+        characterRigidbody = this.GetComponent<Rigidbody>(); //get rigidbody
         characterTransform = this.GetComponent<Transform>(); //get transform
+        characterCollider = this.GetComponent<Collider>(); //get collider
         health = maxHealth; //set health
     }
 
@@ -30,12 +40,13 @@ public class Character : MonoBehaviour
     void FixedUpdate()
     {
         handleMovement();
+        handleJump();
         handleAngle();
         handleWeapons();
 
         if (health <= 0)
             die();
-        else if (maxHealth < health) //if there character has too much health for some reason
+        else if (maxHealth < health) //if the character has too much health for some reason
             health = maxHealth;
 
         moveCharacter(movement);
@@ -49,10 +60,10 @@ public class Character : MonoBehaviour
         Ray ray = new Ray(transform.position, direction); //shoot a ray from current position in direction of travel
         RaycastHit hit;
         if (!Physics.Raycast(ray, out hit, (direction * speed * Time.deltaTime).magnitude)) //if the ray didn't hit anything within the range that we're moving
-            rb.MovePosition((Vector3)transform.position + direction * speed * Time.deltaTime); //go ahead and move
+            characterRigidbody.MovePosition((Vector3)transform.position + direction * speed * Time.deltaTime); //go ahead and move
         else
         {
-            rb.MovePosition(hit.point);//otherwise, move to where the thing we hit was
+            characterRigidbody.MovePosition(hit.point);//otherwise, move to where the thing we hit was
         }
     }
 
@@ -61,6 +72,11 @@ public class Character : MonoBehaviour
     protected virtual void handleMovement()
     {
         movement = new Vector3(0, 0, 0); //don't try to move anywhere
+    }
+
+    protected virtual void handleJump()
+    {
+        //do nothing
     }
 
     protected virtual void handleAngle()
@@ -78,5 +94,18 @@ public class Character : MonoBehaviour
     {
         //this will get a good deal more complicated eventually, but right now it's simple
         Destroy(this.gameObject);
+    }
+
+
+    protected void OnCollisionEnter(Collision collision)
+    {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "staticEnvironment")
+            canJump = true;
+    }
+
+    protected void OnCollisionExit(Collision collision)
+    {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "staticEnvironment")
+            canJump = false;
     }
 }
--- Dance_of_Warriors/Assets/Characters/Player.cs
+++ Dance_of_Warriors/Assets/Characters/Player.cs
@@ -7,13 +7,36 @@ public class Player : Character
 	protected override void handleMovement()
 	{
 
-		if (Input.GetAxis("Forward_Backward") != 0 && Input.GetAxis("Left_Right") != 0)
-			diagonal = true;
+		if (Input.GetAxis("Forward_Backward") != 0 && Input.GetAxis("Left_Right") != 0) //if you're trying to go in two perpendicular directions
+			diagonal = true; //you're trying to move diagonally
 		else
 			diagonal = false;
 
 		movement = new Vector3(Input.GetAxis("Left_Right"), 0, Input.GetAxis("Forward_Backward"));
 		if (diagonal)
-			movement = movement * 0.7f;
+			movement = movement * 0.7f; //if you're moving diagonally, you need to shorten the movement vector
+	}
+
+
+    protected override void handleJump()
+    {
+		if (Input.GetAxis("Jump") != 0 && canJump) //if we want to jump and actually can
+		{
+			//characterRigidbody.useGravity = false;
+			characterRigidbody.AddForce(transform.up * jumpForce);
+			//jumpLeft = jumpHeight;
+			//Debug.Log("jumpLeft = " + jumpLeft);
+
+            //no need to turn off canJump since there's a onCollisionExit function for that
+
+            //while (Input.GetAxis("Jump") != 0 && jumpLeft > 0)
+            //{
+            //    characterRigidbody.useGravity = false;
+            //    jumpLeft--;
+            //}
+            //characterRigidbody.useGravity = true;
+        }
+
+		//jumpLeft = 0;
 	}
 }
