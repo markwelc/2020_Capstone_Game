4cdfb729f0bd61b5e6c3d5567a478ec0136a4940
Hartwelg <43598755+Hartwelg@users.noreply.github.com>
2021-01-02 18:21:33 -0800

    Revert "Revert "update some files""
    This reverts commit 75849336e86f02bc862a6fa5ea34478f193a8cf1.

---
+++ Dance_of_Warriors/Assets/Inventory.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public bool inventoryEnabled = false;
+    public GameObject inventory;
+
+    private int allSlots, enabledSlots;
+    private GameObject[] slot;
+    public GameObject slotHolder;
+
+    void Start()
+    {
+        allSlots = 30;
+        slot = new GameObject[allSlots];
+        for (int i = 0; i < allSlots; i++)
+        {
+            slot[i] = slotHolder.transform.GetChild(i).gameObject;
+        }
+    }
+
+    void Update()
+    {
+        // if (Input.GetButtonDown(KeyCode.Tab))
+        if (Input.GetButtonDown("tab"))
+        {
+            Debug.Log("Tab pressed");
+            inventoryEnabled = !inventoryEnabled;
+        }
+
+        if (inventoryEnabled == true)
+        {
+            inventory.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            inventory.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
