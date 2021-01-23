2b84d33a63ab36daa6eff8f611f46b52a0e94fa5
Gauge <hartwelg@oregonstate.edu>
2021-01-22 16:11:01 -0800

    added health pack, can be added to inventory

---
+++ Dance_of_Warriors/Assets/HealthPack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPack : MonoBehaviour, IInventoryItem
+{
+    public string Name
+    {
+        get
+        {
+            return "Halth Pack";
+        }
+    }
+
+    public Sprite _Image = null;
+
+    public Sprite Image
+    {
+        get
+        {
+            return _Image;
+        }
+    }
+
+    public void OnPickup()
+    {
+        Debug.Log("Health Pack picked up");
+        gameObject.SetActive(false);
+    }
+}
