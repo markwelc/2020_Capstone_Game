a0068ebcf34793cdddb14679d58c49edadcbb305
Gauge <hartwelg@oregonstate.edu>
2021-02-03 15:32:38 -0800

    healing works, selecting from inventory works

--- Dance_of_Warriors/Assets/HealthPack.cs
+++ Dance_of_Warriors/Assets/HealthPack.cs
@@ -33,11 +33,37 @@ public class HealthPack : MonoBehaviour, IInventoryItem
         heal();
     }
 
-    public PlayerHealthController PlayerHealth;
+    Character player;
+    float playerHealth;
+    [SerializeField] public string ourTarget = "Player";
+
+    void Start()
+    {
+        //make an array of all the characters in the scene
+        Character[] myCharacters = FindObjectsOfType(typeof(Character)) as Character[];
+        //for each character in the array
+        foreach(Character person in myCharacters) {
+            //if the current person is the one we are after (the player)
+            if(person.gameObject.name == ourTarget)
+            {
+                //set player to person
+                player = person;
+                //exit loop
+                break;
+            }
+        }
+        //get the player's health
+        playerHealth = player.getHealth();
+    }
+
     public float healVal;
 
     void heal()
     {
-        PlayerHealth.healGeneralHealth(healVal);
+        Debug.Log("Current health: " + player.playerHealthManager.getHealth());
+        // add healVal to player's health
+        player.playerHealthManager.healGeneralHealth(healVal);
+        Debug.Log("Healing player for " + healVal + " health");
+        Debug.Log("New health value: " + player.playerHealthManager.getHealth());
     }
 }
