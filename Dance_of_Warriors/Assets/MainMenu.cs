5ead087c2aa9c281751f7f4cd37d4db1fb16e163
tremperb <55967770+tremperb@users.noreply.github.com>
2021-02-20 20:15:27 -0800

    threw together UI menu
    Initial menu options: play(will load into staging area), info(Can just load a new UI for info on game and warnings), and quit(which will actually exit the application, this will on take noticeable effect on builds)

---
+++ Dance_of_Warriors/Assets/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip selectSound;
+    public AudioClip clickSound;
+    private int playCount = 0;
+
+    private void Start()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+    }
+
+    public void PlayGame()
+    {
+        audioSource.PlayOneShot(clickSound);
+        Debug.Log("Playing Game --> Transfer to staging scene");
+    }
+
+    public void viewInfo()
+    {
+        audioSource.PlayOneShot(clickSound);
+        Debug.Log("View game info and warnings");
+    }
+
+    public void quitApplication()
+    {
+        audioSource.PlayOneShot(clickSound);
+        Debug.Log("Application Closing...");
+    }
+
+    public void playOnSelectSound()
+    {
+        playCount++;
+
+        // Just because I dont want it to play on scene load
+        if(playCount > 1) 
+            audioSource.PlayOneShot(selectSound);
+    }
+
+}
